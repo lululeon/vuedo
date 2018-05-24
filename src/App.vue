99511e057c09a7cc50143c4c49883c7fef17e91b
lululeon <thelululeon@gmail.com>
2018-05-24 15:19:18 -0400

    finished basic vuex/store impl, fixed download to reflect actual values

--- src/App.vue
+++ src/App.vue
@@ -44,12 +44,60 @@ const store = new Vuex.Store({
       state.tasks = initPayload.tasks;
       state.executionLog = initPayload.executionLog;
       state.sentimentLog = initPayload.sentimentLog;
+
+      //descending order of events necessary for chronology-based logic:
+      if(state.executionLog.length > 1) {
+        state.executionLog.sort((a, b) => {
+          const datetimestringA = a.timestamp.toLowerCase();
+          const datetimestringB = b.timestamp.toLowerCase();
+          //MDN: If compareFunction(a, b) is less than 0, sort a to an index lower than b, i.e. a comes first. Hence '>' => reverse sort.
+          if (datetimestringA > datetimestringB) {
+            return -1;
+          }
+          return 1;
+        });
+      }
+      if(state.sentimentLog.length > 1) {
+        state.sentimentLog.sort((a, b) => {
+          const datetimestringA = a.timestamp.toLowerCase();
+          const datetimestringB = b.timestamp.toLowerCase();
+          //MDN: If compareFunction(a, b) is less than 0, sort a to an index lower than b, i.e. a comes first. Hence '>' => reverse sort.
+          if (datetimestringA > datetimestringB) {
+            return -1;
+          }
+          return 1;
+        });
+      }
+    },
+    newExecutionLog (state, logItem) {
+      Vue.set(state, 'executionLog', [logItem, ...state.executionLog]); //preserve descending order (latest at top)
+    },
+    undoExecutionLogByTaskId (state, taskId) { //because of ordering, this should find the latest first
+      let logList = state.executionLog;
+      for(let i=0; i<logList.length; i++){
+        const logItem = logList[i];
+        if (logItem.taskId === taskId) {
+          //delete item and exit loop
+          logList.splice(i, 1);
+          break;
+        }
+      }
+      Vue.set(state, 'executionLog', logList);
     }
   },
   getters: {
-    tasks (state) {
-      return state.tasks;
-    }
+    // goals (state) {
+    //   return state.goals;
+    // },
+    // tasks (state) {
+    //   return state.tasks;
+    // },
+    // executionLog (state) {
+    //   return state.executionLog;
+    // },
+    // sentimentLog (state) {
+    //   return state.sentimentLog;
+    // }
   }
 });
 // <<<
