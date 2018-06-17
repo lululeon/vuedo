fb852122cd55e4438e49ee13318bc03da5f2d413
lululeon <thelululeon@gmail.com>
2018-06-17 15:15:16 -0400

    sass'd task css, more tweaks to collapsible tasks

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -2,7 +2,7 @@
   <article class="task">
     <header class="task-header">
       <EditableText class="task-header-title" :content="tasktitle" @updated="onUpdateDescription" />
-      <span  class="ministatus" v-if="!expanded">{{ runningTotal }} {{ metric }} {{ thisTimeframeText }} </span>
+      <span  class="ministatus" :class="runningTotalStyling" v-if="!expanded">{{ runningTotal }} {{ metric }} {{ thisTimeframeText }} </span>
       <div class="task-header-ctrlbtn">
         <a @click="decrement" aria-label="decrement count">
           <span class="icon"><font-awesome-icon :icon="['fas', 'minus']" /></span>
@@ -23,19 +23,19 @@
           <span class="icon"><font-awesome-icon :icon="['fas', 'caret-down']" /></span>
         </a>
       </div>
+      <div class="task-header-ctrlbtn deleter">
+        <a @click="deleteTask" aria-label="delete">
+          <span class="icon"><font-awesome-icon :icon="['fas', 'trash-alt']" /></span>
+        </a>
+      </div>
     </header>
     <div class="task-body" v-if="expanded">
       <div class="task-meta-cells">
         <div class="task-meta-cell small"><span>id</span><span>{{ task.id }}</span></div>
         <div class="task-meta-cell large" :class="{'is-active': editingTargets}" @click="editTargets"><span>target</span><span>{{ task.metric.measureTarget }} {{ metric }} {{ timeframe }}</span></div>
-        <div class="task-meta-cell"><span>step size</span><span>{{ task.metric.stepSize }} {{ stepSizeMetricLabel }}</span></div>
+        <div class="task-meta-cell large"><span>step size</span><span>{{ task.metric.stepSize }} {{ stepSizeMetricLabel }}</span></div>
         <div class="task-value-cell" :class="runningTotalStyling" ><span>running total</span><span>{{ runningTotal }} {{ metric }} {{ thisTimeframeText }} </span></div>
       </div>
-    </div>
-    <div class="task-footer buttons" v-if="expanded">
-      <button class="button primary is-danger is-outlined" @click="deleteTask">
-        <span class="icon"><font-awesome-icon :icon="['fas', 'trash-alt']" /></span>
-      </button>
     </div>
     <modal v-if="editingTargets"/>
   </article>
@@ -163,61 +163,149 @@ export default {
     runningTotalStyling() {
       const rt = this.runningTotal; //display units, not raw units.
       const target = this.task.metric.measureTarget; //display units, not raw units.
-      if(rt > 0) {
-        if(rt < target) {
+      const failingLimit = 0.2*target;
+      const slackingLimit = 0.4*target;
+      const progressingLimit = 0.75*target;
+      const achievingLimit = 0.95*target;
+      const achievedLimit = target;
+      const exceedingLimit = 1.1*target;
+      if (rt > 0) {
+        if (rt < failingLimit) {
+          return 'failing';
+        }
+        if (rt < slackingLimit) {
+          return 'slacking';
+        }
+        if (rt < progressingLimit) {
           return 'progressing';
         }
-        return 'achieving';
+        if (rt < achievingLimit) {
+          return 'achieving';
+        }
+        if (rt < achievedLimit) {
+          return 'achieved';
+        }
+        if( rt > exceedingLimit) {
+          return 'exceeding';
+        }
+        return 'achieved';
       }
-      return '';
+      return 'notstarted';
     }
   },
-
   mounted() {
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import './node_modules/bulma/bulma.sass';
+
+/* progress and status colors - may move to a more central location later */
+$status-notstarted: $danger;
+$status-failing: #ff583b;
+$status-slacking:#ff800f;
+$status-progressing: #ffc107;
+$status-achieving: #bfc22c;
+$status-achieved: #8bc34a;
+$status-exceeding: $turquoise;
+
+.notstarted {
+  background-color: $status-notstarted;
+}
+.failing {
+  background-color: $status-failing;
+}
+.slacking {
+  background-color: $status-slacking;
+}
+.progressing {
+  background-color: $status-progressing;
+}
+.achieving {
+  background-color: $status-achieving;
+}
+.achieved {
+  background-color: $status-achieved;
+}
+.exceeding {
+  background-color: $status-exceeding;
+}
+
 .task {
-  border: 1px solid #eee;
-  padding: 1rem;
   margin: 0.8rem 0;
   border-radius: 0.3rem;
+  background-color: $light;
 }
+/* =================== TASK HEADER =================== */
 .task-header {
   display: flex;
   align-items: center;
-  background-color: #f3f3f3;
+  background-color: $light;
   border-radius: 0.3rem;
   padding: 1rem;
-  margin-bottom: 0.5rem;
+  @media screen and (max-width: 450px) {
+    flex-wrap: wrap;
+  }
 }
 .task-header-title {
   flex-grow: 2;
   font-size: 1.75rem;
+  @media screen and (max-width: 450px) {
+    font-size: 1.25rem;
+    margin-bottom: 0.5rem;
+    width: 100%;
+    padding: 0.25rem;
+  }
 }
 .task-header-ctrlbtn {
   font-size: 1.5rem;
   border-radius: 0.3rem;
   margin: 0.2rem;
-  background-color: #fff;
+  background-color: $white;
+  &.deleter>a {
+    color: $danger;
+  }
+  &>a {
+    color: $grey;
+    display: inline-block;
+    padding:0.5rem;
+  }
+  &>a .icon {
+    display: grid; /* force centered, square tiles */
+    font-size: small;
+  }
 }
 .ministatus {
-  background-color: #9E9E9E;
   padding: 0.5rem;
   border-radius: 0.3rem;
   margin: 0.2rem;
-  color: #fff;
+  color: $white;
+  &.notstarted { @extend .notstarted; }
+  &.failing { @extend .failing; }
+  &.slacking { @extend .slacking; }
+  &.achieving { @extend .achieving; }
+  &.achieved { @extend .achieved; }
+  &.exceeding { @extend .exceeding; }
+  @media screen and (max-width: 450px) {
+    width: 100%;
+  }
 }
+/* =================== TASK BODY =================== */
 .task-body {
-  margin-bottom: 0.5rem;
+  margin: 0.5rem 0;
+  padding: 0 0.5rem;
 }
 .task-meta-cells {
   display:flex;
   padding: 0.2rem;
   border-radius: 0.3rem;
-  background-color: #cecece;
+  background-color: $light;
+  @media screen and (max-width: 450px) {
+    flex-direction: column;
+    margin:0;
+    border-radius: 0;
+  }
 }
 .task-meta-cell, .task-value-cell {
   text-align: center;
@@ -225,89 +313,64 @@ export default {
   margin: 0.15rem;
   display: flex;
   flex-direction: column;
-  /*border: 1px solid #eee;*/
   border-radius: 0.3rem;
-  background-color: #fff;
-}
-.task-meta-cell.small {
-  width: 50px;
-}
-.task-meta-cell.large {
-  width: 150px;
 }
 .task-value-cell {
   width: 200px;
-  color: #fff;
-  background-color: #9e9e9e;
-}
-.task-value-cell.achieving {
-  background-color: #8bc34a;
-}
-.task-value-cell.progressing {
-  background-color: #dca500;
-}
-.task-value-cell.behind { /* not used yet */
-  background-color: #f44336;
-}
-.task-meta-cell span, .task-value-cell span {
+  color: $white;
+  span {
     padding: 0.5rem;
-}
-.task-meta-cell>span:first-child {
-  background-color: #f3f3f3;
-  border-radius: 0.2rem 0.2rem 0 0;
-  font-weight: bold;
-}
-.task-value-cell>span:first-child {
-  background-color: #3e3e3e;
-  border-radius: 0.2rem 0.2rem 0 0;
-}
-.task-header-ctrlbtn.expander {
-  background-color: #ffdd57;
-}
-.task-header-ctrlbtn>a {
-  color: #717171;
-  display: inline-block;
-  padding:0.5rem;
-}
-.task-header-ctrlbtn>a .icon {
-  display: grid; /* force centered, square tiles */
-  font-size: small;
-}
-.task-footer .button {
-  border-width: 2px;
-}
+    &:first-child {
+      background-color: $grey-dark;
+      border-radius: 0.2rem 0.2rem 0 0;
+    }
+  }
+  &.notstarted { @extend .notstarted; }
+  &.failing { @extend .failing; }
+  &.slacking { @extend .slacking; }
+  &.achieving { @extend .achieving; }
+  &.achieved { @extend .achieved; }
+  &.exceeding { @extend .exceeding; }
 
-@media screen and (max-width: 450px) { /*when width gets less than this...*/
-  .task-header {
-    flex-wrap: wrap;
-  }
-  .task-header-title {
-    font-size: 1.25rem;
-    margin-bottom: 0.5rem;
-    width: 100%;
-    padding: 0.25rem;
-  }
-  .task-meta-cells {
-    flex-direction: column;
-    margin:0;
-    border-radius: 0;
-  }
-  .ministatus {
-    width:100%;
-  }
-  .task-meta-cell, .task-meta-cell.small, .task-meta-cell.large,
-  .task-value-cell {
+  @media screen and (max-width: 450px) {
     flex-direction: row;
     width: 100%;
     margin:0;
     border-radius:0;
+    & span {
+      flex-basis: 0;
+      flex-grow: 1;
+    }
   }
-  .task-meta-cell span, .task-value-cell span {
-    flex-basis: 0;
-    flex-grow: 1;
+}
+.task-meta-cell {
+  background-color: $white;
+  &.small {
+    min-width: 50px;
   }
-  .task-meta-cell>span:first-child, .task-value-cell>span:first-child {
-    border-radius: 0.2rem 0 0 0.2rem;
+  &.large {
+    min-width: 150px;
+    flex: 1 1 auto;
+  }
+  span {
+    padding: 0.5rem;
+    &:first-child {
+      border-radius: 0.2rem 0.2rem 0 0;
+      font-weight: bold;
+    }
+  }
+  @media screen and (max-width: 450px) {
+    flex-direction: row;
+    width: 100%;
+    margin:0;
+    border-radius:0;
+    & span {
+      flex-basis: 0;
+      flex-grow: 1;
+      &:first-child {
+        border-radius: 0;
+      }
+    }
   }
 }
 </style>
