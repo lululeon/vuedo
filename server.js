dae32b0052a5743511d68a7640c44252d3c04567
lululeon <thelululeon@gmail.com>
2018-08-16 21:46:00 -0400

    made exp,serve-static dev deps, added log msg to debug heroku

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-console */
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
-app.listen(port);
+app.listen(port, () => {
+  console.log(`vuedo-proto on port ${port} Listening!`);
+});
