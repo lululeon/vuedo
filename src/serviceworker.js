9c612ac916f1c1dc355153c28cf28e3d2093da17
lululeon <thelululeon@gmail.com>
2018-11-17 00:28:09 -0500

    ability to create indexdb

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -1,4 +1,41 @@
 // NOTE!! This file MUST be built first, and then incorporated into the webpack build pipeline (see vue.config.js) ...
 // ...in order to spit out the actual /service-worker.js !!
 
-console.log('Hello from the service worker file !! :-)'); // eslint-disable-line
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+/* eslint-disable no-useless-escape */
+
+import idb from 'idb';
+
+function createDB() {
+  idb.open('vuedodb', 1, function(upgradeDB) {
+    var store = upgradeDB.createObjectStore('tasks', {
+      keyPath: 'id'
+    });
+    store.put({id: 123, name: 'coke', price: 10.99, quantity: 200});
+    store.put({id: 321, name: 'pepsi', price: 8.99, quantity: 100});
+    store.put({id: 222, name: 'water', price: 11.99, quantity: 300});
+  });
+}
+
+function readDB() {
+  idb.open('tasks', 1).then(function(db) {
+    var tx = db.transaction(['tasks'], 'readonly');
+    var store = tx.objectStore('tasks');
+    return store.getAll();
+  }).then(function(items) {
+    // Use beverage data
+    items.forEach(item => console.log("found:", item))
+  });
+}
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    createDB()
+  );
+});
+
+/* eslint-enable no-useless-escape */
+/* eslint-enable no-undef */
+/* eslint-enable no-console */
+
